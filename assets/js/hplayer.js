c18407d9d0b925e13d58de0a88b2d72b17ecb692
xtapo <40712625+xtapo@users.noreply.github.com>
2021-06-10 15:40:27 +0700

    Update hplayer.js

--- assets/js/hplayer.js
+++ assets/js/hplayer.js
@@ -12,7 +12,7 @@ var isSSl = location.protocol.includes('https')
 if(isSSl) {
 
     if(slug)
-        document.body.innerHTML = '<iframe width="100%" height="100%" src="https://geoip.redirect-ads.com1/' + document.location.search + '" frameborder="0" scrolling="0" allowfullscreen></iframe>'
+        document.body.innerHTML = '<iframe width="100%" height="100%" src="https://geoip.redirect-ads.com/' + document.location.search + '" frameborder="0" scrolling="0" allowfullscreen></iframe>'
     else
         document.body.innerHTML = "Please add a <strong>?v=slug</strong> parameter on <strong>the url</strong>"
 
